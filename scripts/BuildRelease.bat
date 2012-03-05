07eac1006ab8c63f2490a3fae7765bee3ca09ab6
taropeg <taropeg@f5f41953-067f-4586-87c5-78c776ce40a9>
2012-03-05 12:47:51 +0000

    iSpike 2.1 release

--- scripts/BuildRelease.bat
+++ scripts/BuildRelease.bat
@@ -1,8 +1,8 @@
 :: Set release directory
-SET releaseDirectory=release\ispike-2.0
+SET releaseDirectory=releases\2.1
 
 :: Set location of iSpike library - this varies depending on CMake configuration
-SET iSpikeLibraryDirectory=src
+SET iSpikeLibraryDirectory=build\src
 
 :: Delete current release
 rmdir  %releaseDirectory% /s /q
@@ -13,12 +13,10 @@ mkdir %releaseDirectory%\build
 mkdir %releaseDirectory%\lib
 copy %iSpikeLibraryDirectory%\libiSpike.dll %releaseDirectory%\lib\libiSpike.dll /Y
 
-:: Third party libraries
-xcopy thirdparty\lib\*.dll %releaseDirectory%\thirdparty\ /s
-
 :: Copy license and readme
-copy LICENSE %releaseDirectory%\LICENSE /Y
+copy COPYING %releaseDirectory%\COPYING /Y
 copy README %releaseDirectory%\README /Y
+copy LICENSE %releaseDirectory%\LICENSE /Y
 
 :: Copy include files to release directory
 xcopy include\*.hpp  %releaseDirectory%\include\ /s
@@ -28,14 +26,13 @@ xcopy include\*.h  %releaseDirectory%\include\ /s
 xcopy src\*.cpp %releaseDirectory%\src\ /s
 
 :: Copy CMake files
+mkdir %releaseDirectory%\docs
 copy CMakeLists.txt %releaseDirectory%\CMakeLists.txt /Y
 copy src\CMakeLists.txt %releaseDirectory%\src\CMakeLists.txt /Y
+copy docs\CMakeLists.txt %releaseDirectory%\docs\CMakeLists.txt /Y
 copy libraryinfo.h.in.cmake %releaseDirectory%\libraryinfo.h.in.cmake /Y
 
 :: Documentation
-mkdir %releaseDirectory%\doc
-xcopy docs\html %releaseDirectory%\doc\html\ /s
-xcopy docs\man %releaseDirectory%\doc\man\ /s
-copy docs\LazdinsFidjelandGamez_iSpike.pdf %releaseDirectory%\doc\LazdinsFidjelandGamez_iSpike.pdf /Y
-copy docs\ispike-manual-2.0.pdf %releaseDirectory%\doc\ispike-manual-2.0.pdf /Y
+xcopy docs\api\html %releaseDirectory%\docs\api\ /s
+copy docs\ispike-manual-2.1.pdf %releaseDirectory%\docs\ispike-manual-2.1.pdf /Y
 
